4021a3a91dd1f8638a9a98636a805c533f9ab9b2
liaowentao <liaowentao@sightp.com>
2019-04-15 21:17:43 +0800

    fix

--- BabylonTSProjectTemplateCreationTool.bat
+++ BabylonTSProjectTemplateCreationTool.bat
@@ -1,14 +1,14 @@
 @echo off
 call npm init -y
 call npm install --save-dev typescript webpack ts-loader webpack-cli
-call npm install --save babylonjs
 
-@echo const path = require("path");                       > webpack.config.js
+
+@echo const path = require("path");                       >> webpack.config.js
 @echo module.exports = {                                  >> webpack.config.js
-@echo     entry: './assets/scripts/index.ts',                            >> webpack.config.js
+@echo     entry: './assets/scripts/index.ts',             >> webpack.config.js
 @echo     output: {                                       >> webpack.config.js
 @echo         filename: 'index.js',                       >> webpack.config.js
-@echo         path: path.resolve(__dirname, 'bulid/outfile')       >> webpack.config.js
+@echo         path: path.resolve(__dirname, 'src')        >> webpack.config.js
 @echo     },                                              >> webpack.config.js
 @echo     resolve: {                                      >> webpack.config.js
 @echo         extensions: [".ts"]                         >> webpack.config.js
@@ -18,7 +18,9 @@ call npm install --save babylonjs
 @echo             { test: /\.tsx?$/, loader: "ts-loader" }>> webpack.config.js
 @echo         ]                                           >> webpack.config.js
 @echo     },                                              >> webpack.config.js
-@echo     mode: "development"                             >> webpack.config.js
+@echo     mode: "development",                            >> webpack.config.js
+@echo     devtool: "source-map",                          >> webpack.config.js
+@echo     watch: false                                    >> webpack.config.js
 @echo };                                                  >> webpack.config.js
 
 @echo {                                  >tsconfig.json
@@ -38,11 +40,8 @@ call npm install --save babylonjs
 @echo       "es5"                        >>tsconfig.json 
 @echo     ],                             >>tsconfig.json 
 @echo     "declaration": true,           >>tsconfig.json 
-@echo     "outDir": "./bulid/outfile"    >>tsconfig.json 
-@echo   },                               >>tsconfig.json 
-@echo   "files": [                       >>tsconfig.json 
-@echo     "./assets/scripts/index.ts"                   >>tsconfig.json 
-@echo   ]                                >>tsconfig.json 
+@echo     "outDir": "./src"             >>tsconfig.json 
+@echo   }                                >>tsconfig.json 
 @echo }                                  >>tsconfig.json 
 
 
@@ -71,40 +70,51 @@ call npm install --save babylonjs
 @echo=                                                       >>index.html
 @echo ^<body^>                                               >>index.html
 @echo     ^<canvas id="renderCanvas"^>^</canvas^>            >>index.html
-@echo     ^<script src="bulid/outfile/index.js"^>^</script^> >>index.html
+@echo     ^<script src="src/index.js"^>^</script^>          >>index.html
 @echo ^</body^>                                              >>index.html
 @echo=                                                       >>index.html
 @echo ^</html^>                                              >>index.html
+
 md "assets"
 cd assets
 md "scripts"
 cd scripts
-@echo import * as BABYLON from 'babylonjs';                                                                      >>index.ts
-@echo import { Engine, Scene, ArcRotateCamera, HemisphericLight, Vector3, MeshBuilder, Mesh } from "babylonjs";  >>index.ts
-@echo=                                                                                                           >>index.ts
-@echo var canvas: any = document.getElementById("renderCanvas");                                                 >>index.ts
-@echo var engine: Engine = new Engine(canvas, true);                                                             >>index.ts
-@echo=                                                                                                           >>index.ts
-@echo function createScene(): Scene {                                                                            >>index.ts
-@echo     var scene: Scene = new Scene(engine);                                                                  >>index.ts
-@echo=                                                                                                           >>index.ts
-@echo     var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);                              >>index.ts
-@echo     camera.attachControl(canvas, true);                                                                    >>index.ts
-@echo=                                                                                                           >>index.ts
-@echo     var light1: HemisphericLight = new HemisphericLight("light", new Vector3(1, 1, 0), scene);             >>index.ts
-@echo=                                                                                                           >>index.ts
-@echo     var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);                         >>index.ts
-@echo=                                                                                                           >>index.ts
-@echo     return scene;                                                                                          >>index.ts
-@echo }                                                                                                          >>index.ts
-@echo=                                                                                                           >>index.ts
-@echo var scene: Scene = createScene();                                                                          >>index.ts
-@echo=                                                                                                           >>index.ts
-@echo engine.runRenderLoop(() =^> {                                                                              >>index.ts
-@echo     scene.render();                                                                                        >>index.ts
-@echo });                                                                                                        >>index.ts
+
+@echo import { Engine, Scene, ArcRotateCamera, Vector3, MeshBuilder, Mesh, DirectionalLight } from "babylonjs";>>index.ts
+@echo class Program {>>index.ts
+@echo     private static smCanvas: HTMLCanvasElement = null;>>index.ts
+@echo     private static smEngine: Engine = null;>>index.ts
+@echo     public static main() {>>index.ts
+@echo         this.smCanvas = document.getElementById("renderCanvas") as HTMLCanvasElement;>>index.ts
+@echo         this.smEngine = new Engine(this.smCanvas, true, { preserveDrawingBuffer: true }, true);>>index.ts
+@echo         this.smEngine.setHardwareScalingLevel(0.5);>>index.ts
+@echo= >>index.ts
+@echo         var scene: Scene = this._createScene(this.smEngine, this.smCanvas);>>index.ts
+@echo         this.smEngine.runRenderLoop(() =^> {>>index.ts
+@echo             scene.render();>>index.ts
+@echo         });>>index.ts
+@echo         window.addEventListener("resize", () =^> {>>index.ts
+@echo             this.smEngine.resize();>>index.ts
+@echo         })>>index.ts
+@echo     }>>index.ts
+@echo     private static _createScene(engine: Engine, canvas: HTMLCanvasElement): Scene {>>index.ts
+@echo         var scene: Scene = new Scene(engine);>>index.ts
+@echo= >>index.ts
+@echo         var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);>>index.ts
+@echo         camera.attachControl(canvas, true);>>index.ts
+@echo= >>index.ts
+@echo         var light: DirectionalLight = new DirectionalLight("light", new Vector3(-1, -1, 0), scene);>>index.ts
+@echo= >>index.ts
+@echo         var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);>>index.ts
+@echo= >>index.ts
+@echo         return scene;>>index.ts
+@echo     }>>index.ts
+@echo }>>index.ts
+@echo Program.main();>>index.ts
+
 cd..
 cd..
+
 @echo off&setlocal enabledelayedexpansion
 setlocal enableDelayedExpansion
 for /f "eol=* tokens=*" %%i in (package.json) do (
